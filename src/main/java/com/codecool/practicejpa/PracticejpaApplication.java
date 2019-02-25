53fc633d66cbfb71ffa53b53c40336b3ab89bc4e
TribTrob <trib073@gmail.com>
2019-02-25 09:47:48 +0100

    Initial commit

---
+++ src/main/java/com/codecool/practicejpa/PracticejpaApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.practicejpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticejpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticejpaApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/codecool/practicejpa/entity/Episode.java
@@ -0,0 +1,24 @@
+package com.codecool.practicejpa.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class Episode {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+
+}
